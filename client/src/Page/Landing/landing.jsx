e0dba823d0999875cd533785a8ccee77309bc48a
Stark <123723458+St4rkXc@users.noreply.github.com>
2024-12-01 23:26:01 +0800

    navbar & tailwind config setup

--- client/src/Page/Landing/landing.jsx
+++ client/src/Page/Landing/landing.jsx
@@ -1,40 +1,47 @@
 import Logo from "../../assets/image/Logo.png";
+// eslint-disable-next-line no-unused-vars
 import Mentor1 from "../../assets/image/Mentor image-1.png";
 const Landing = () => {
-  return (
-    <>
-      {/* Navbar  */}
-      <div className="navbar bg-base-100 pl-5 pr-5">
-        <div className="navbar-start">
-          <a className="btn btn-ghost text-xl font-Open-sauce font-normal">
-            <img src={Logo} alt="" />
-            Koursian
-          </a>
-        </div>
-        <div className=" navbar-center">
-          <div className=" p-5">
-            <a role="button" className="btn ">
-              Link
-            </a>
-            <a role="button" className="btn ml-3">
-              Link
-            </a>
-            <a role="button" className="btn ml-3">
-              Link
-            </a>
-            <a role="button" className="btn ml-3">
-              Link
-            </a>
-          </div>
-        </div>
-
-        <div className="navbar-end">
-          <a className="btn">Button</a>
-        </div>
-      </div>
-      {/* Nav end  */}
-    </>
-  );
+    return (
+        <>
+            {/* Navbar  */}
+            <nav className="navbar bg-base-100 py-5 container mx-auto">
+                <div className="navbar-start">
+                    <div className="flex gap-4 items-center">
+                        <img src={Logo} alt="" />
+                        <p className="text-3lg text-text font-semibold">Koursian</p>
+                    </div>
+                </div>
+                <div className=" navbar-center">
+                    <div className="flex gap-3">
+                        <a href="" className="nav-link bg-primary text-white font-medium opacity-100">
+                            Home
+                        </a>
+                        <a href="" className="nav-link">
+                            Bootcamp
+                        </a>
+                        <a href="" className="nav-link">
+                            Advantage
+                        </a>
+                        <a href="" className="nav-link">
+                            Testimonials
+                        </a>
+                    </div>
+                </div>
+                <div className="navbar-end">
+                    <div className="flex items-center gap-4">
+                        <a href="" className="text-text font-semibold">
+                            Sign Up
+                        </a>
+                        <a href="" className="px-12 py-3 font-semibold flex items-center justify-center border-2 text-text  border-primary rounded-3xl hover:bg-primary hover:text-white duration-200">
+                            Sign In
+                        </a>
+                    </div>
+                </div>
+            </nav>
+            {/* Nav end  */}
+        </>
+    );
 };
 
 export default Landing;
