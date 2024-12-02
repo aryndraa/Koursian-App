80a64c5f011491cae3cc834e12d2def09c9b735c
Stark <123723458+St4rkXc@users.noreply.github.com>
2024-12-02 08:41:44 +0800

    navbar compo & trust section

--- client/src/Page/Landing/landing.jsx
+++ client/src/Page/Landing/landing.jsx
@@ -1,61 +1,69 @@
 /* eslint-disable no-unused-vars */
 import Logo from "../../assets/image/Logo.png";
 import Mentor1 from "../../assets/image/Mentor image-1.png";
-import deco from "../../assets/image/deco.svg";
+import Mentor2 from "../../assets/image/Mentor image-2.png";
+import Mentor3 from "../../assets/image/Mentor image-3.png";
+import Mentor4 from "../../assets/image/Mentor image-4.png";
+import Mentor5 from "../../assets/image/Mentor image.png";
+import Deco from "../../assets/image/deco.svg";
+import Navbar from "../../components/landing/navbar.jsx";
 const Landing = () => {
     return (
         <>
-            {/* Navbar  */}
-            <nav className="navbar bg-base-100 py-5 container mx-auto">
-                <div className="navbar-start">
-                    <div className="flex gap-4 items-center">
-                        <img src={Logo} alt="" />
-                        <p className="text-3lg text-text font-semibold">Koursian</p>
-                    </div>
-                </div>
-                <div className=" navbar-center">
-                    <div className="flex gap-3">
-                        <a href="" className="nav-link bg-primary text-white font-medium opacity-100">
-                            Home
-                        </a>
-                        <a href="" className="nav-link">
-                            Bootcamp
-                        </a>
-                        <a href="" className="nav-link">
-                            Advantage
-                        </a>
-                        <a href="" className="nav-link">
-                            Testimonials
-                        </a>
-                    </div>
-                </div>
-                <div className="navbar-end">
-                    <div className="flex items-center gap-4">
-                        <a href="" className="text-text font-semibold">
-                            Sign Up
-                        </a>
-                        <a href="" className="px-12 py-3 font-semibold flex items-center justify-center border-2 text-text  border-primary rounded-3xl hover:bg-primary hover:text-white duration-200">
-                            Sign In
-                        </a>
-                    </div>
-                </div>
-            </nav>
-            {/* Nav end  */}
-            <head className="flex flex-col justify-center items-center mt-14">
-                <img src={deco} alt="" />
+            <Navbar />
+            <head className="flex flex-col items-center justify-center mt-14">
+                <img src={Deco} alt="" />
                 <h1 className="text-6xl font-bold text-text leading-[110px] text-center max-w-[1188px]">
                     The <span className="text-secondary">future</span> belongs to those who keep <span className="text-primary">learning</span>{" "}
                 </h1>
                 <p className="text-text font-medium text-base mt-8 max-w-[524px] text-center opacity-80">
                     Every skill you gain brings new opportunities and pushes you closer to your goals. Start today with Koursian
                 </p>
-                <a href="" className="py-3 pl-8 pr-3 bg-primary text-white font-semibold mt-8 rounded-3xl flex justify-center items-center gap-4">
+                <a href="" className="flex items-center justify-center gap-4 py-3 pl-8 pr-3 mt-8 font-semibold text-white bg-primary rounded-3xl">
                     Get Started
-                    <div className="flex justify-center items-center bg-white rounded-full p-1">
-                        <span className="solar-arrow-landing text-primary font-bold"></span>
+                    <div className="flex items-center justify-center p-1 bg-white rounded-full">
+                        <span className="font-bold solar-arrow-landing text-primary"></span>
                     </div>
                 </a>
             </head>
+            <div className="hidden mt-16 2xl:block">
+                <p className="text-base font-medium text-center text-text/60">Meet Future Mentor</p>
+                <div className="items-center justify-center hidden gap-5 mt-8 2xl:flex">
+                    <img src={Mentor1} alt="mentor1" />
+                    <img src={Mentor2} alt="mentor2" />
+                    <img src={Mentor3} alt="mentor3" />
+                    <img src={Mentor4} alt="mentor4" />
+                    <img src={Mentor5} alt="mentor5" className="hidden 2xl:block" />
+                </div>
+            </div>
+            <section className="w-full p-16 mt-16 bg-primary">
+                <div className="container flex flex-wrap items-center justify-between mx-auto">
+                    <div className="text-center">
+                        <h2 className="text-5xl font-bold text-white">10+</h2>
+                        <p className="text-base font-bold text-white opacity-40">Years of Service</p>
+                    </div>
+                    <div className="devider-vertical"></div>
+                    <div className="text-center">
+                        <h2 className="text-5xl font-bold text-white">10+</h2>
+                        <p className="text-base font-bold text-white opacity-40">Years of Service</p>
+                    </div>
+                    <div className="devider-vertical"></div>
+                    <div className="text-center">
+                        <h2 className="text-5xl font-bold text-white">10+</h2>
+                        <p className="text-base font-bold text-white opacity-40">Years of Service</p>
+                    </div>
+                    <div className="devider-vertical"></div>
+                    <div className="text-center">
+                        <h2 className="text-5xl font-bold text-white">10+</h2>
+                        <p className="text-base font-bold text-white opacity-40">Years of Service</p>
+                    </div>
+                    <div className="devider-vertical"></div>
+                    <div className="text-center">
+                        <h2 className="text-5xl font-bold text-white">10+</h2>
+                        <p className="text-base font-bold text-white opacity-40">Years of Service</p>
+                    </div>
+                </div>
+            </section>
         </>
     );
 };
---
+++ client/src/components/landing/navbar.jsx
@@ -0,0 +1,44 @@
+import Logo from "../../assets/image/Logo.png"
+
+const navbar = () =>{
+    return(
+        <>
+        <nav className="container py-5 mx-auto navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="flex items-center gap-4">
+                        <img src={Logo} alt="" />
+                        <p className="font-semibold text-3lg text-text">Koursian</p>
+                    </div>
+                </div>
+                <div className=" navbar-center">
+                    <div className="flex gap-3">
+                        <a href="" className="font-medium text-white opacity-100 nav-link bg-primary">
+                            Home
+                        </a>
+                        <a href="" className="nav-link">
+                            Bootcamp
+                        </a>
+                        <a href="" className="nav-link">
+                            Advantage
+                        </a>
+                        <a href="" className="nav-link">
+                            Testimonials
+                        </a>
+                    </div>
+                </div>
+                <div className="navbar-end">
+                    <div className="flex items-center gap-4">
+                        <a href="" className="font-semibold text-text">
+                            Sign Up
+                        </a>
+                        <a href="" className="flex items-center justify-center px-12 py-3 font-semibold duration-200 border-2 text-text border-primary rounded-3xl hover:bg-primary hover:text-white">
+                            Sign In
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default navbar
