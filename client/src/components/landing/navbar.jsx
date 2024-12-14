746b5c0897bad1296069fa5490b8deeb4d428870
Stark <123723458+St4rkXc@users.noreply.github.com>
2024-12-14 21:10:21 +0800

    landing new fix

--- client/src/components/landing/navbar.jsx
+++ client/src/components/landing/navbar.jsx
@@ -1,9 +1,9 @@
-import Logo from "../../assets/image/Logo.png"
+import Logo from "../../assets/image/Logo.png";
 
-const navbar = () =>{
-    return(
+const navbar = () => {
+    return (
         <>
-        <nav className="container py-5 mx-auto navbar bg-base-100">
+            <nav className="container py-5 mx-auto navbar bg-base-100">
                 <div className="navbar-start">
                     <div className="flex items-center gap-4">
                         <img src={Logo} alt="" />
@@ -11,7 +11,7 @@ const navbar = () =>{
                     </div>
                 </div>
                 <div className=" navbar-center">
-                    <div className="flex gap-3">
+                    <div className="items-center hidden gap-3 xl:flex">
                         <a href="" className="font-medium text-white opacity-100 nav-link bg-primary">
                             Home
                         </a>
@@ -27,7 +27,7 @@ const navbar = () =>{
                     </div>
                 </div>
                 <div className="navbar-end">
-                    <div className="flex items-center gap-4">
+                    <div className="items-center hidden gap-4 xl:flex">
                         <a href="" className="font-semibold text-text">
                             Sign Up
                         </a>
@@ -35,10 +35,44 @@ const navbar = () =>{
                             Sign In
                         </a>
                     </div>
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="text-white border-none outline-none btn bg-primary lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul className="menu menu-sm dropdown-content space-y-4 bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li>
+                                <a href="" className="nav-link">
+                                    Bootcamp
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" className="nav-link">
+                                    Advantage
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" className="nav-link">
+                                    Testimonials
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" className="text-base font-semibold text-text">
+                                    Sign Up
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" className="font-semibold text-white bg-primary">
+                                    Sign in
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </>
-    )
-}
+    );
+};
 
-export default navbar
+export default navbar;
