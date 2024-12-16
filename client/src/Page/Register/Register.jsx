87177916e798ea7da1ec798f88cb4c74d2295b31
erza-503 <rendymahardika2007@gmail.com>
2024-12-16 14:07:22 +0800

    update register

--- client/src/Page/Register/Register.jsx
+++ client/src/Page/Register/Register.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
 import Layout2 from "../layout/Layout2";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
-  return <Layout2></Layout2>;
+const {password, setPassword} = useState("")
+
+  const dummy ={
+    email:"",
+    username:"",
+    password:{password}
+  }
+
+  const {register, handlesubmit} =useForm();
+
+   const onSubmit = (data) => {
+    const {username, password, email} = data
+
+  }
+
+  return (
+  <Layout2
+  main={
+  <div>
+
+  </div>
+  }
+  >
+  </Layout2>
+  );
 };
 
 export default Register;
